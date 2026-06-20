b2603a8098a68eeacf08ed379d8ab670a3ed2599
Saranya <saranlavan18@gmail.com>
2026-06-20 15:53:41 +0530

    initial commit - food app

---
+++ lib/splash_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'splash_screen.dart';
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // wait 3 seconds, then move to next screen
+    Future.delayed(const Duration(seconds: 3), () {
+      // we will add navigation to Login Screen here once it's ready
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepOrange,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.fastfood,
+              size: 100,
+              color: Colors.white,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'QuickBite',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
